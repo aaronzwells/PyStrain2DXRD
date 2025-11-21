cdce0faacf0aad5651f7e68f33cdbebb0fbdba50
Benjamin Schneiderman <benjaminschneiderman@Benjamins-MacBook-Pro.local>
2025-11-21 13:57:41 -0700

    Added masking logic for Pilatus

--- 1.FindingRefPeaks.py
+++ 1.FindingRefPeaks.py
@@ -6,12 +6,10 @@ import pyFAI
 import fabio
 import os
 
-poni_file = "calibration/Calibration_LaB6_100x100_3s_r8_mod2.poni" # calibration PONI file
-<<<<<<< HEAD
-tif_file = "InputFiles/VB-APS-SSAO-6_25C_TestMap-AO_000288.avg.tiff" # representative data TIF file
-=======
-tif_file = "InputFiles/25C_AO_inputs/VB-APS-SSAO-6_25C_Map-AO_000288.avg.tiff" # representative data TIF file
->>>>>>> origin
+poni_file = "calibration/Calibration_Oct25_ceria_900mm_linkam_30C_att000_0006091.poni" # calibration PONI file
+tif_file = "InputFiles/VB_APS_SSAO_5_25C_Before_AO/VB_APS_SSAO_5_25C_Before_AO_0000907.tif" # representative data TIF file
+detector_type = "Pilatus" # "Pilatus" or "GE"
+mask_file = "InputFiles/pilatus_mask.msk" # Either "path/to/your/mask.tif" or None
 
 def main(
         poni_file=poni_file, 
@@ -32,10 +30,17 @@ def main(
 
     # Load image data from .tif
     image = fabio.open(tif_file).data
+    image = np.flipud(image) if detector_type == "Pilatus" else image   # Flip the image vertically - MATCHES Oct. 25 CALIBRATION
+
+
+    # Load mask if provided
+    if mask_file:
+        mask = fabio.open(mask_file).data.astype(bool)
+
 
     # Perform azimuthal integration to get 1D pattern (q vs I)
-    npt = 2000
-    result = ai.integrate1d(image, npt, unit="q_nm^-1")
+    npt = 2000 # number of radial bins
+    result = ai.integrate1d(image, npt, mask=mask, unit="q_nm^-1")
     q = result.radial
     I = result.intensity
 
@@ -66,7 +71,7 @@ def main(
     ax = plt.gca()
     ax.xaxis.set_major_locator(ticker.MultipleLocator(10))
     ax.xaxis.set_minor_locator(ticker.AutoMinorLocator(5))
-    ax.set_xlim(10,90)
+    ax.set_xlim(0,90)
     
     # plt.legend()
     plt.tight_layout()
