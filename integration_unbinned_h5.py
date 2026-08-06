551d17095fdc40ad0008cad9e72d732835655f16
Ben Schneiderman <ben.schneiderman@hysafillers.com>
2026-08-05 19:33:03 -0600

    Working .hdf5 writer for unbinned integration

--- integration_unbinned_h5.py
+++ integration_unbinned_h5.py
@@ -17,8 +17,8 @@ from matplotlib import ticker
 import pyFAI
 import fabio
 import os
+import logging
 import h5py
-from pyFAI.io import HDF5Writer
 
 
 ################# OVERARCHING SAMPLE NOTES ##########################
@@ -52,67 +52,40 @@ scan_range = (169, 520) #Beamtime assigned scan IDs
 
 # Reference path: InputFiles/Feb2025_OnHeat_25C/VB-APS-SSAO-6_25C_TestMap-AO_000169.avg.tiff
 
+def setup_logger(log_path, logger_name=None):
+    logger = logging.getLogger(logger_name)
+    
+    # Clear existing handlers to avoid duplicates
+    if logger.hasHandlers():
+        logger.handlers.clear()
+
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    fh = logging.FileHandler(log_path)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    # Add StreamHandler for console output
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    return logger
+
+
 def get_tif_file(scan_id):
     return os.path.join(local_folder, isolated_mapscan_location, f"{beamtime_given_prefix}{scan_id:06d}.avg.tiff")
-
+    
 
 def main():
-
+    
     # Create output location for the single .h5, outside the main loop
     output_path = os.path.join("1_UnbinnedIntegration_PeakFinding", visit, isolated_mapscan_location)
-    fl.create_directory(output_path, exist_ok=True)
+    fl.create_directory(output_path)
 
-    # Intialize .hdf5 writer: single file per map scan
-    h5_path = os.path.join(output_path, f"scan_range_{scan_range[0]}_{scan_range[1]}.h5")
-    writer = HDF5Writer(filename=h5_path)
-    writer.init_file()
-
-    # Inclusive range from scan_range[0] to scan_range[1]
-    for scan_id in range(scan_range[0], scan_range[1] + 1):
-        indiv_file_path = get_tif_file(scan_id)
-
-        # Skip missing frame files gracefully if a scan in the range was aborted/missing
-        if not os.path.exists(indiv_file_path):
-            print(f"Warning: File not found for scan {scan_id:06d}, skipping...")
-            continue
-
-        # Load image data
-        image = fabio.open(indiv_file_path).data
-        # EXTREMELY IMPORTANT: Flip the image AND MASK vertically for Pilatus detector: MATCHES Oct. 25 CALIBRATION
-        image = np.flipud(image) if detector_type == "Pilatus" else image   
-
-        # Perform 1D pyFAI integration
-        npt = 2000 # number of radial bins
-        result = ai.integrate1d(image, npt, mask=mask, dummy=np.nan, unit="q_nm^-1")
-
-        # Format entry_name using zero-padded scan_id to keep HDF5 keys sorted naturally
-        entry_key = f"scan_{scan_id:06d}"
-
-        # Append integration result into the open .h5 file
-        writer.write(result, entry_name=entry_key)
-
-    # Close the HDF5 writer once all scans are processed
-    writer.close()
-    print(f"UNBINNED INTEGRATION: Successfully saved scans {scan_range[0]} through {scan_range[1]} into: {h5_path}")
-
-
-    #######AARON Script 1########
-
-    # This removes the file extension and .avg from the end of the averaged image files
-    filename = fl.remove_filename_extension(tif_file)
-    
-    # Creates an output directory of the same name as the mapping image to store all the data for that map image location
-    outputPath = os.path.join("1_UnbinnedIntegration_PeakFinding", visit, descriptor, filename)
-    output_path = fl.create_directory(outputPath)
-    print(f"[INFO] Output Path is {output_path}")
-
-    # Load the pyFAI integrator from the .poni calibration file
-    ai = pyFAI.load(poni_file)
-
-    # Load image data from .tif
-    image = fabio.open(tif_file).data
-    # EXTREMELY IMPORTANT: Flip the image AND MASK vertically for Pilatus detector: MATCHES Oct. 25 CALIBRATION
-    image = np.flipud(image) if detector_type == "Pilatus" else image   
+    # --- Setup file logger for pipeline ---
+    log_path = os.path.join(output_path, f"scan_range_{scan_range[0]}_{scan_range[1]}.log")
+    file_logger = setup_logger(log_path, logger_name = f"scan_range_{scan_range[0]}_{scan_range[1]}.log")
 
     # Load mask if provided
     if mask_file:
@@ -122,51 +95,97 @@ def main():
     else:
         mask = None
 
-    # Perform azimuthal integration to get 1D pattern (q vs I)
-    npt = 2000 # number of radial bins
-    writer = HDF5Writer(filename=f"{output_path}/unbinned_integration.h5", h5path="entry/pyFAI")
-    writer.init_file()
-    result = ai.integrate1d(image, npt, mask=mask, dummy=np.nan, unit="q_nm^-1")
-    writer.write(result, entry_name=filename)
-    writer.close()
-    q = result.radial
-    I = result.intensity
+    # Intialize .hdf5 writer: single file per map scan
+    h5_path = os.path.join(output_path, f"scan_range_{scan_range[0]}_{scan_range[1]}.h5")
+    if os.path.exists(h5_path):
+        file_logger.info(f"UNBINNED INTEGRATION: HDF5 file already exists for scans {scan_range[0]} through {scan_range[1]}, skipping...")
+        return
 
-    # Save temporary file to use with validate_curve_fitting()
-    # temp_int_file = "temp_intensity.int"
-    # np.savetxt(temp_int_file, np.column_stack((q, I, np.zeros_like(q))), fmt="%.6f")
+    else:
+        # Inclusive range from scan_range[0] to scan_range[1]
+        print("\n")
+        for scan_id in range(scan_range[0], scan_range[1] + 1):
+            indiv_file_path = get_tif_file(scan_id)
 
-    # Call validate_curve_fitting to fit peaks
-    peak_positions_q = fl.fit_peak_centroids(q, I, height_frac=height_frac, distance=distance)
-    peak_positions_d = 2 * np.pi / np.array(peak_positions_q)
-    # os.remove(temp_int_file)
+            # Skip missing frame files gracefully if a scan in the range was aborted/missing
+            if not os.path.exists(indiv_file_path):
+                print(f"Warning: File not found for scan {scan_id:06d}, skipping...")
+                continue
 
-    # Save peaks to file
-    output_txt = os.path.join(output_path,"peak_positions.txt")
-    np.savetxt(output_txt, np.column_stack((peak_positions_q, peak_positions_d)),
-                 fmt="%.6f", delimiter="\t", header="q [nm^-1] \t d [nm]")
-    print(f"Detected {len(peak_positions_q)} peaks. Saved to {output_txt}")
+            # Load image data
+            image = fabio.open(indiv_file_path).data
+            # EXTREMELY IMPORTANT: Flip the image AND MASK vertically for Pilatus detector: MATCHES Oct. 25 CALIBRATION
+            image = np.flipud(image) if detector_type == "Pilatus" else image   
 
-    # Save q & I data to a file
-    np.savetxt(f"{output_path}/q_vs_I.txt", np.column_stack((q, I)), fmt="%.6f", delimiter=" ")
+            # Perform 1D pyFAI integration
+            ai = pyFAI.load(poni_file)
+            npt = 2000 # number of radial bins
+            result = ai.integrate1d(image, npt, mask=mask, dummy=np.nan, unit="q_nm^-1")
+            q = result.radial
+            intensity = result.intensity
 
-    # Plot the pattern and detected peaks
-    plt.figure(figsize=(5, 3))
-    plt.plot(q, I, label='Integrated pattern', linewidth=1.0, color='k')
-    plt.plot(peak_positions_q, [np.interp(p, q, I) for p in peak_positions_q], 'rx', label='Fitted Peaks')
-    plt.xlabel("q [nm$^{-1}$]")
-    plt.ylabel("Intensity [a.u.]")
-    # plt.title("1D Azimuthally Integrated Pattern with Peak Locations")
+            # Format entry_name using zero-padded scan_id to keep HDF5 keys sorted naturally
+            entry_key = f"scan_{scan_id:06d}"
 
-    ax = plt.gca()
-    ax.xaxis.set_major_locator(ticker.MultipleLocator(10))
-    ax.xaxis.set_minor_locator(ticker.AutoMinorLocator(5))
-    ax.set_xlim(10,90)
-    
-    # plt.legend()
-    plt.tight_layout()
-    plt.savefig(f"{output_path}/peak_detection_plot.png", dpi=600)
-    # plt.show()
+           # Create entry group inside .h5 file
+            entry_group = h5f.require_group(entry_key)
+
+            # Write dataset arrays
+            dset_q = entry_group.create_dataset("q", data=q, compression="gzip")
+            dset_q.attrs["units"] = "nm^-1"
+
+            dset_i = entry_group.create_dataset("intensity", data=intensity, compression="gzip")
+            dset_i.attrs["units"] = "a.u."
+
+            # Set NeXus plottable standards (enables direct plotting in silx view / PyMca)
+            entry_group.attrs["NX_class"] = "NXdata"
+            entry_group.attrs["signal"] = "intensity"
+            entry_group.attrs["axes"] = "q"
+            file_logger.info(f"UNBINNED INTEGRATION: Done with scan {scan_id}")
+
+    print("\n")
+    file_logger.info(f"\nUNBINNED INTEGRATION: Successfully saved scans {scan_range[0]} through {scan_range[1]} into: {h5_path}")
 
 if __name__ == "__main__":
     main()
+
+
+
+    ####### From AARON Script 1 ########
+    
+#     # Save temporary file to use with validate_curve_fitting()
+#     # temp_int_file = "temp_intensity.int"
+#     # np.savetxt(temp_int_file, np.column_stack((q, I, np.zeros_like(q))), fmt="%.6f")
+
+#     # Call validate_curve_fitting to fit peaks
+#     peak_positions_q = fl.fit_peak_centroids(q, I, height_frac=height_frac, distance=distance)
+#     peak_positions_d = 2 * np.pi / np.array(peak_positions_q)
+#     # os.remove(temp_int_file)
+
+#     # Save peaks to file
+#     output_txt = os.path.join(output_path,"peak_positions.txt")
+#     np.savetxt(output_txt, np.column_stack((peak_positions_q, peak_positions_d)),
+#                  fmt="%.6f", delimiter="\t", header="q [nm^-1] \t d [nm]")
+#     print(f"Detected {len(peak_positions_q)} peaks. Saved to {output_txt}")
+
+#     # Save q & I data to a file
+#     np.savetxt(f"{output_path}/q_vs_I.txt", np.column_stack((q, I)), fmt="%.6f", delimiter=" ")
+
+#     # Plot the pattern and detected peaks
+#     plt.figure(figsize=(5, 3))
+#     plt.plot(q, I, label='Integrated pattern', linewidth=1.0, color='k')
+#     plt.plot(peak_positions_q, [np.interp(p, q, I) for p in peak_positions_q], 'rx', label='Fitted Peaks')
+#     plt.xlabel("q [nm$^{-1}$]")
+#     plt.ylabel("Intensity [a.u.]")
+#     # plt.title("1D Azimuthally Integrated Pattern with Peak Locations")
+
+#     ax = plt.gca()
+#     ax.xaxis.set_major_locator(ticker.MultipleLocator(10))
+#     ax.xaxis.set_minor_locator(ticker.AutoMinorLocator(5))
+#     ax.set_xlim(10,90)
+    
+#     # plt.legend()
+#     plt.tight_layout()
+#     plt.savefig(f"{output_path}/peak_detection_plot.png", dpi=600)
+#     # plt.show()
+
